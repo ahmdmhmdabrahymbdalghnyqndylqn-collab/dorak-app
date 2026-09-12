b10ff89c3646617e4907d3d39d91307fcbb760ee
ahmdmhmdabrahymbdalghnyqndylqn-collab <ahmdmhmdabrahymbdalghnyqndylqn@gmail.com>
2026-09-12 19:21:35 +0300

    Add src/theme.js

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+export const colors = {
+  primary: '#0F4C5C',
+  primaryDark: '#0A3540',
+  primarySoft: '#EAF4F6',
+  white: '#FFFFFF',
+  canvas: '#F6F8F9',
+  text: '#17262C',
+  muted: '#718087',
+  line: '#E6ECEE',
+  green: '#27AE60',
+  greenSoft: '#EAF8F0',
+  orange: '#F2A93B',
+  orangeSoft: '#FFF6E7',
+  red: '#D9544D',
+  redSoft: '#FDEEEE',
+  blue: '#3C7DA0',
+};
+
+export const shadow = {
+  shadowColor: '#0A3540',
+  shadowOpacity: 0.07,
+  shadowRadius: 18,
+  shadowOffset: { width: 0, height: 7 },
+  elevation: 3,
+};
